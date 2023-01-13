289ab05de2c15c3da26b4a84d680a2f284af8d1f
AHenry2117 <aehenry2117@gmail.com>
2023-01-12 20:36:05 -0800

    added db migrations and models

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,19 +1,27 @@
 #config
 from flask import Flask 
+from flask_migrate import Migrate
 
 def create_app(): 
     app = Flask(__name__)
 
+    from . import pet,fact,models
+
+    #config
+    app.config["TEMPLATES_AUTO_RELOAD"] = True
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres@localhost:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+ 
+    #database integration
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
+
+    #register blueprint
+    app.register_blueprint(pet.bp)
+    app.register_blueprint(fact.bp)
+
     @app.route('/')
     def index(): 
         return 'Hello, PetFax!'
-
-    #register pet blueprint
-    from . import pet 
-    app.register_blueprint(pet.bp)
-
-    #register fact blueprint
-    from . import fact
-    app.register_blueprint(fact.bp)
-
+    
     return app
